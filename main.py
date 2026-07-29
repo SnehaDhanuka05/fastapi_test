8242d7c525baa2e18c0ac9767cfe8fd00ae39360
SnehaDhanuka05 <10c.snehadhanuka@gmail.com>
2026-07-29 10:36:50 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI 
+from pydantic import BaseModel 
+from datetime import datetime
+import mysql.connector
+import base64
+
+app = FastAPI()
+
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+    print("Connected to MySQL database")
+
+except mysql.connector.Error as err:
+    print(f"Error connecting to MySQL database: {err}")
+
+class Post(BaseModel):
+    post_id:int
+    title:str
+    content:str
+    image_url:str
+    created_at:datetime
+    likes:int
+
+@app.get("/")
+async def root():
+    return {"message":"is this working"}
+
+@app.get("/posts")
+async def get_posts(post:Post):
+    #encoded_post = base64.b64encode(post.json())
+    return post
+    
