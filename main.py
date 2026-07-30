940a4d2643e8624b75a18437fba715367f42cf4a
SnehaDhanuka05 <10c.snehadhanuka@gmail.com>
2026-07-30 16:09:59 +0530

    adding the images to db and fetching it

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import base64
 from functools import cache
 import time
 from pathlib import Path
+import asyncio
 
 app = FastAPI()
 
@@ -26,7 +27,7 @@ async def root():
     return {"message":"is this working"}
 
 
-image_dir = Path("/Users/sneha/fastapi/images")
+#image_dir = Path("/Users/sneha/fastapi/images")
 
 @app.get("/posts")
 async def get_posts():
@@ -35,43 +36,27 @@ async def get_posts():
         user="root",
         port=3306,
         password="1234",
-        database="sys"
+        database="fastapi_task"
         )
     print("Connected to MySQL database")
     #removed try and except, connection was showing as undefined variable
     cur = connection.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS posts (post_id INT PRIMARY KEY, title VARCHAR(255), content TEXT, image_url VARCHAR(255), created_at DATETIME, likes INT)")
-    cur.execute("INSERT INTO posts(post_id, title, content, image_url, created_at, likes) VALUES (1, 'best food in town', 'awesome restaurants to try!', '/Users/sneha/fastapi/images.jpeg', NOW(), 5)")
-    posts=cur.execute("SELECT * FROM posts")
-    posts=cur.fetchall()
+    #cur.execute("CREATE TABLE IF NOT EXISTS posts (post_id INT PRIMARY KEY, title VARCHAR(255), content TEXT, image_url VARCHAR(255), created_at DATETIME, likes INT)")
+    #cur.execute("INSERT INTO posts(post_id, title, content, image_url, created_at, likes) VALUES (1, 'best food in town', 'awesome restaurants to try!', '/Users/sneha/fastapi/images.jpeg', NOW(), 5)")
+    #posts=cur.execute("SELECT * FROM posts")
+    #posts=cur.fetchall()
 
-#used AI to get this block of code to bulk insert urls in the table
-    for i, image_path in enumerate(image_dir.iterdir(), start=2):
-        if image_path.is_file():
-            cur.execute(
-            """
-            INSERT INTO posts (post_id, title, content, image_url, created_at, likes)
-            VALUES (%s, %s, %s, %s, NOW(), %s)
-            """,
-            (
-                i,
-                f"Image {i}",
-                "Image from folder",
-                str(image_path),   # or image_path.name
-                0
-            )
-        )
+ #used AI to get this block of code to bulk insert urls in the table
+    
 
     image_urls=[]
-    image_urls=cur.execute("SELECT image_url FROM posts")
+    image_urls=cur.execute("SELECT image_url FROM new_post")
     image_urls=cur.fetchall()
-    print(image_urls)
 
     start_time = time.time()
     encoded_posts=[]
-    for urls in image_dir.iterdir():
-        print(urls)
-        encoded_posts.append(await convert_base64(urls))
+    for urls in image_urls:
+        encoded_posts.append(await convert_base64(urls[0]))
     end_time = time.time()
 
     print(f"Time taken: {end_time - start_time}")
@@ -82,9 +67,9 @@ async def get_posts():
 #for stale cache, we can use TTL ofc but also learnt about Debezium. uses kafka to stream chnages from logs of db
 #for mysql, aiomysql can be used
 
+
 #@cache
 async def convert_base64(image_url: str):
-    print(image_url)
     with open(image_url, "rb") as img:
         s=base64.b64encode(img.read()).decode("utf-8")
-    return s 
+    return s
